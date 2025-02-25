33608a8f51be4f5da67401460ea892bd8b7bd2cd
ManuelMoralesN <manuela.edson11@gmail.com>
2025-02-25 11:26:28 -0600

    Buffer Class Implementation
    25/02 implementacion de la clase buffer

---
+++ KamogawaEngine-/Include/Buffer.h
@@ -0,0 +1,54 @@
+#pragma once
+#include "Prerequisites.h"
+
+class Device;
+class DeviceContext;
+class MeshComponent;
+
+class Buffer
+{
+public:
+    Buffer() = default;
+    ~Buffer() = default;
+
+   
+    HRESULT 
+    init(Device& device, 
+         const MeshComponent& mesh, 
+         unsigned int bindFlag);
+
+    HRESULT
+    init(Device& device, 
+         unsigned int ByteWidth);
+    
+    void 
+    update(DeviceContext& deviceContext, 
+           unsigned int DstSubresource,
+           const D3D11_BOX* pDstBox,
+           const void* pSrcData,
+           unsigned int SrcRowPitch,
+           unsigned int SrcDepthPitch);
+
+    void 
+    render(DeviceContext& deviceContext,
+           unsigned int StartSlot,
+           unsigned int NumBuffers,
+           DXGI_FORMAT format = DXGI_FORMAT_UNKNOWN,
+           bool setPixelShader = false);
+
+   
+    void 
+    destroy();
+
+private:
+    HRESULT
+        createBuffer(Device& device,
+                     D3D11_BUFFER_DESC& desc,
+                     D3D11_SUBRESOURCE_DATA* initData);
+
+private:
+    ID3D11Buffer* m_buffer = nullptr;
+    unsigned int m_stride = 0;
+    unsigned int m_offset = 0;
+    unsigned int m_bindFlag = 0;
+};
